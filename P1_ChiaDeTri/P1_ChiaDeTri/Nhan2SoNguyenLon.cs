1e62df1814a5afec439c719f95fe3fa7b9934deb
hoangtranlenaa <roynguyenna@gmail.com>
2024-07-17 10:27:05 +0700

    Add File Code and File Video

---
+++ P1_ChiaDeTri/P1_ChiaDeTri/Nhan2SoNguyenLon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace P1_ChiaDeTri
+{
+    class nhan2SoNguyenLon
+    {
+        public static void Main(string[] args)
+        {
+            string num1 = "12345678901234567890";
+            string num2 = "98765432109876543210";
+            BigInteger result = MultiplyBigIntegers(num1, num2);
+            Console.WriteLine("Result: "+result);
+            Console.ReadLine();
+        }
+
+        public static BigInteger MultiplyBigIntegers(string num1, string num2)
+        {
+            if (num1 == "0" || num2 == "0")
+                return 0;
+
+            if (num1.Length < 4 || num2.Length < 4)
+                return new BigInteger(long.Parse(num1)) * new BigInteger(long.Parse(num2));
+
+            int mid1 = num1.Length / 2;
+            int mid2 = num2.Length / 2;
+
+            string a = num1.Substring(0, mid1);
+            string b = num1.Substring(mid1);
+            string c = num2.Substring(0, mid2);
+            string d = num2.Substring(mid2);
+
+            BigInteger p1 = MultiplyBigIntegers(a, c);
+            BigInteger p2 = MultiplyBigIntegers(a, d);
+            BigInteger p3 = MultiplyBigIntegers(b, c);
+            BigInteger p4 = MultiplyBigIntegers(b, d);
+
+            return p1 * (BigInteger)Math.Pow(10, mid1 * 2) + (p2 + p3) * (BigInteger)Math.Pow(10, mid1) + p4;
+
+        }
+
+    }
+}
---
+++ P1_ChiaDeTri/P1_ChiaDeTri/timKiemNhiPhan.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace P1_ChiaDeTri
+//{
+//    class timKiemNhiPhan
+//    {
+//        public static int BinarySearch(int[] arr, int key)
+//        {
+//            int left = 0;
+//            int right = arr.Length - 1;
+
+//            while (left <= right)
+//            {
+//                int mid = left + (right - left) / 2;
+
+//                if (arr[mid] == key)
+//                {
+//                    return mid;
+//                }
+//                else if (arr[mid] < key)
+//                {
+//                    left = mid + 1;
+//                }
+//                else
+//                {
+//                    right = mid - 1;
+//                }
+//            }
+
+//            return -1;
+//        }
+//        static void Main(string[] args)
+//        {
+//            int[] numbers = { 1, 3, 5, 7, 9, 11, 13, 15 };
+//            int index = BinarySearch(numbers, 7);
+//            Console.WriteLine("Phan tu 7 xuat hien o vi tri so " + index);
+//            Console.ReadLine();
+//        }
+//    }
+//}
